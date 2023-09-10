85ba50ede74437db5b3948dd25729b0e7c9fc651
Roni <ronniiii@yahoo.com>
2023-09-10 19:18:50 +0000

    [FEATURE]add emailjs for form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,39 @@
-import React from "react";
+import { useRef, useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ery0cp8",
+        "template_e6ej3qt",
+        form.current,
+        "wLhnusKVW2bFhZfIx"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <main id="contact">
       <h1>Contact</h1>
-      <form>
+      <form ref={form} onSubmit={sendEmail}>
         <div className="g-af">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Your Email" />
+          <input name="name" type="text" placeholder="Your Name" />
+          <input name="email" type="email" placeholder="Your Email" />
         </div>
-        <textarea placeholder="Your Message"></textarea>
+        <input type="text" name="subject" placeholder="Subject" />
+        <textarea name="message" placeholder="Your Message"></textarea>
         <button className="button button-primary">Send</button>
       </form>
     </main>
